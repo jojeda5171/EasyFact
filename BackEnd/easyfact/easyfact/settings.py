b2866ecae7084a1a191221d8b7aa99c670fc57ee
José Ojeda <jojeda5171@uta.edu.ec>
2023-06-09 21:54:22 -0500

    erreglo

--- BackEnd/easyfact/easyfact/settings.py
+++ BackEnd/easyfact/easyfact/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^&3ei(iso)zm_b1awn@k6(4ki^iepjg-#kv(f+vw=-mm!$a@zd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'jojeda.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -124,7 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
